1e380b99593a7a960bc0d2d2208bbc9104518926
Hafijul Islam <hafijul233@gmail.com>
2020-11-19 11:22:26 +0600

    second assignment

---
+++ c1w4/assignment2-2.py
@@ -0,0 +1,9 @@
+# Write a program that uses input to prompt a user for their
+# name and then welcomes them. Note that input will pop up
+# a dialog box. Enter Sarah in the pop-up box when you are
+# prompted so your output will match the desired output.
+
+# The code below almost works
+
+name = input("Enter your name")
+print("Hello", name)
